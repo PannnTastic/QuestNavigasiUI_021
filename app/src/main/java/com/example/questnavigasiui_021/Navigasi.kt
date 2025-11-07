1d0863a8237ca013bb6bd3723d361ee0fe0cc697
PannnTastic <pannntastic@gmail.com>
2025-11-07 15:40:57 +0700

    add Navigasi.kt as navhost and set the navcontroller

--- app/src/main/java/com/example/questnavigasiui_021/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_021/Navigasi.kt
@@ -1,5 +1,51 @@
 package com.example.questnavigasiui_021
 
-enum class Navigasi{
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasiui_021.view.FormIsian
+import com.example.questnavigasiui_021.view.TampilData
 
+enum class Navigasi{
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
+    Scaffold() { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ){
+            composable(route = Navigasi.Formulir.name) {
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {cancelAndBackToFormulir(navController)}
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+){
+    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
 }
