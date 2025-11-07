5a3a6e811acaf0c0f3376f4ae3918dbec83e2cf8
PannnTastic <pannntastic@gmail.com>
2025-11-07 15:00:21 +0700

    Add fun FormIsian

--- app/src/main/java/com/example/questnavigasiui_021/view/FormIsian.kt
+++ app/src/main/java/com/example/questnavigasiui_021/view/FormIsian.kt
@@ -1,2 +1,13 @@
 package com.example.questnavigasiui_021.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jk:List<String> = listOf("Laki-Laki","Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+
+}
