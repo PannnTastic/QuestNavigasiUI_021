056c90b261cae1d24dcaebf752f52e68c17bc385
PannnTastic <pannntastic@gmail.com>
2025-11-07 15:08:03 +0700

    Add add Scaffold And column and text field for nama lengkap

--- app/src/main/java/com/example/questnavigasiui_021/view/FormIsian.kt
+++ app/src/main/java/com/example/questnavigasiui_021/view/FormIsian.kt
@@ -1,7 +1,22 @@
 package com.example.questnavigasiui_021.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.questnavigasiui_021.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,5 +24,30 @@ fun FormIsian(
     jk:List<String> = listOf("Laki-Laki","Perempuan"),
     OnSubmitBtnClick : () -> Unit
 ){
-
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(stringResource(id = R.string.home))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(id = R.color.teal_700))
+            ) }
+        ) {
+        isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = { }
+            )
+        }
+    }
 }
